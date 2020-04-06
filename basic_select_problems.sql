c59c134bc4699f07f5a3c154242ddce4c5c5e570
Ryan McCall <ryan.e.mccall@gmail.com>
2020-04-06 12:29:17 -0500

    Add more solutions

--- basic_select_problems.sql
+++ basic_select_problems.sql
@@ -1,4 +1,5 @@
 -- https://www.hackerrank.com/challenges/revising-the-select-query/problem
+
 -- Query all columns for all American cities in CITY with populations larger than 100000.
 -- The CountryCode for America is USA.
 SELECT * FROM city 
@@ -6,6 +7,7 @@ WHERE population > 100000
 AND CountryCode = 'USA';
 
 -- https://www.hackerrank.com/challenges/revising-the-select-query-2/problem
+
 -- Query the names of all American cities in CITY with populations larger than 120000.
 -- The CountryCode for America is USA.
 SELECT name FROM city
@@ -13,39 +15,46 @@ WHERE population > 120000
 AND CountryCode = 'USA';
 
 -- https://www.hackerrank.com/challenges/select-all-sql/problem
+
 -- Query all columns (attributes) for every row in the CITY table.
 SELECT * FROM city;
 
 -- https://www.hackerrank.com/challenges/select-by-id/problem
+
 -- Query all columns for a city in CITY with the ID 1661.
 SELECT * FROM city
 WHERE ID = '1661';
 
 -- https://www.hackerrank.com/challenges/japanese-cities-name/problem
+
 -- Query the names of all the Japanese cities in the CITY table. 
 -- The COUNTRYCODE for Japan is JPN.
 SELECT * FROM city
 WHERE CountryCode = 'JPN';
 
 -- https://www.hackerrank.com/challenges/weather-observation-station-1/problem
+
 -- Query a list of CITY and STATE from the STATION table.
 SELECT CITY, STATE FROM STATION;
 
 -- https://www.hackerrank.com/challenges/weather-observation-station-3/problem
+
 -- Query a list of CITY names from STATION with even ID numbers only.
 -- You may print the results in any order, but must exclude duplicates from your answer.
 SELECT DISTINCT CITY FROM STATION
-WHERE ID % 2 = 0
+WHERE ID % 2 = 0;
 
 -- https://www.hackerrank.com/challenges/weather-observation-station-4/problem
+
 -- Let N be the number of CITY entries in STATION, and let N' be the number of distinct
 -- CITY names in STATION; query the value of N-N' from STATION. In other words, find the
 -- difference between the total number of CITY entries in the table and the number of
 -- distinct CITY entries in the table.
 SELECT 
-    (SELECT COUNT(*) FROM STATION) - (SELECT COUNT(DISTINCT CITY) FROM STATION)
+    (SELECT COUNT(*) FROM STATION) - (SELECT COUNT(DISTINCT CITY) FROM STATION);
 
 -- https://www.hackerrank.com/challenges/weather-observation-station-5/problem
+
 -- Query the two cities in STATION with the shortest and longest CITY names, as well as
 -- their respective lengths (i.e.: number of characters in the name). If there is more
 -- than one smallest or largest city, choose the one that comes first when ordered
@@ -59,8 +68,42 @@ ORDER BY LENGTH(CITY) DESC, CITY
 LIMIT 1;
 
 -- https://www.hackerrank.com/challenges/weather-observation-station-6/problem
+
 -- Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION.
 -- Your result cannot contain duplicates.
 SELECT DISTINCT city
 FROM station
 WHERE LEFT(city, 1) IN ("a", "e", "i", "o", "u");
+
+-- https://www.hackerrank.com/challenges/weather-observation-station-7/problem
+
+-- Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. 
+-- Your result cannot contain duplicates.
+SELECT DISTINCT city
+FROM station
+WHERE RIGHT(city, 1) IN ('a', 'e', 'i', 'o', 'u');
+
+-- https://www.hackerrank.com/challenges/weather-observation-station-8/problem
+
+-- Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as
+-- both their first and last characters. Your result cannot contain duplicates.
+SELECT DISTINCT city
+FROM station
+WHERE LEFT(city, 1) IN ("a", "e", "i", "o", "u")
+AND RIGHT(city, 1) IN ('a', 'e', 'i', 'o', 'u');
+
+-- https://www.hackerrank.com/challenges/weather-observation-station-9/problem
+
+-- Query the list of CITY names from STATION that do not start with vowels. Your result
+-- cannot contain duplicates.
+SELECT DISTINCT city
+FROM station
+WHERE LEFT(city, 1) NOT IN ("a", "e", "i", "o", "u");
+
+-- https://www.hackerrank.com/challenges/weather-observation-station-10/problem
+
+-- Query the list of CITY names from STATION that do not end with vowels. Your result
+-- cannot contain duplicates.
+SELECT DISTINCT city
+FROM station
+WHERE RIGHT(city, 1) NOT IN ('a', 'e', 'i', 'o', 'u');
