a52b0c650f97cdfc7bb4617f72e27bc25c911557
Ryan McCall <ryan.e.mccall@gmail.com>
2020-07-04 15:04:08 -0500

    Complete basic select problems

--- basic_select_problems.sql
+++ basic_select_problems.sql
@@ -148,3 +148,15 @@ ORDER BY RIGHT(`name`, 3), id;
 SELECT `name`
 FROM employee
 ORDER BY `name`;
+
+-- https://www.hackerrank.com/challenges/salary-of-employees/problem
+
+-- Write a query that prints a list of employee names (i.e.: the name attribute) for 
+-- employees in Employee having a salary greater than $2000 per month who have been 
+-- employees for less than 10 months. Sort your result by ascending employee_id.
+
+SELECT `name`
+FROM employee
+WHERE salary > 2000
+AND months < 10
+ORDER BY employee_id ASC;
